4c8d54fa6c786f17991178978c2b5af3f2b39f5d
amercamper <84207596+amercamper@users.noreply.github.com>
2024-07-12 22:48:07 +0300

    Update folder.py

--- bot/folder.py
+++ bot/folder.py
@@ -1,27 +1,87 @@
-import os
+from textwrap import dedent
 
-from . import BASE_FOLDER
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+from pyrogram.filters import command, document, media
+from pyrogram.handlers.callback_query_handler import CallbackQueryHandler
+from pyrogram.handlers.message_handler import MessageHandler
+from pyrogram.types import Message
 
-storage = BASE_FOLDER
+from . import DL_FOLDER, download, folder, sysinfo
+from .util import checkAdmins
 
 
-def reset():
-    global storage
-    storage = BASE_FOLDER
+def register(app: Client):
+    # Register message handlers for commands and file/media handling
+    app.add_handler(MessageHandler(start, command('start')))
+    app.add_handler(MessageHandler(botHelp, command('help')))
+    app.add_handler(MessageHandler(usage, command('usage')))
+    app.add_handler(MessageHandler(useFolder, command('use')))
+    app.add_handler(MessageHandler(getFolder, command('get')))
+    app.add_handler(MessageHandler(leaveFolder, command('leave')))
+    app.add_handler(MessageHandler(download.handler.addFile, document | media))
+    
+    # Register callback query handler for download manager
+    app.add_handler(CallbackQueryHandler(download.manager.stopDownload))
 
 
-def set(path: str):
-    global storage
-    storage = os.path.join(BASE_FOLDER, path)
+@checkAdmins
+async def start(_, message: Message):
+    await message.reply(dedent("""
+        Hello!
+        Send me a file and I will download it to my server.
+        If you need help send /help
+    """))
+
+
+@checkAdmins
+async def botHelp(_, message: Message):
+    await message.reply(dedent("""
+        My Commands are:
+
+        /usage: Gets the disk usage
+        /use: Use a specific folder inside storage
+        /get: Gets in which folder I am
+        /leave: Go back to the root of storage
+    """))
+
+
+@checkAdmins
+async def usage(_, message: Message):
     try:
-        os.mkdir(storage)
-    except FileExistsError:
-        pass
+        usage = sysinfo.diskUsage(DL_FOLDER)
+        await message.reply(
+            dedent(f"""
+                The storage path configured has __{usage.capacity}__ of storage
+                Of those, __{usage.used}__ is in use, and __{usage.free}__ is free.
+            """),
+            parse_mode=ParseMode.MARKDOWN
+        )
+    except Exception as e:
+        await message.reply(f"Error fetching disk usage: {e}")
 
 
-def getPath() -> str:
-    return storage.replace(BASE_FOLDER, '', 1) or "/"
+@checkAdmins
+async def useFolder(_, message: Message):
+    args = message.text.split()
+    userSetPath = ' '.join(args[1:]).strip()
+    if not userSetPath:
+        await message.reply("You haven't told me where I need to put your files!")
+        return
+    path = userSetPath.replace('../', '').replace('/..', '')
+    if userSetPath != path:
+        await message.reply(f"Warning: Path is `{path}` not `{' '.join(args[1:])}`")
+    folder.set(path)
+    await message.reply("Ok, send me files now and I will put them in this folder.")
 
 
-def get() -> str:
-    return storage
+@checkAdmins
+async def leaveFolder(_, message: Message):
+    folder.reset()
+    await message.reply("I'm in the root folder again :)")
+
+
+@checkAdmins
+async def getFolder(_, message: Message):
+    path = folder.getPath()
+    await message.reply(f"I'm currently in the `{path}` folder")
