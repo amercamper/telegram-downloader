3b3451627de03d164eb9fe95256e867ee01cf421
amercamper <84207596+amercamper@users.noreply.github.com>
2024-07-12 22:48:55 +0300

    Update sysinfo.py

--- bot/sysinfo.py
+++ bot/sysinfo.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
-
 import psutil
-
 from . import DL_FOLDER, util
-
 
 @dataclass
 class Usage:
@@ -12,12 +9,11 @@ class Usage:
     free: str
     percent: int
 
-
 def diskUsage(folder: str = DL_FOLDER) -> Usage:
     usage = psutil.disk_usage(folder)
     return Usage(
         used=util.humanReadable(usage.used),
         capacity=util.humanReadable(usage.total),
-        free=util.humanReadable(usage.total-usage.used),
+        free=util.humanReadable(usage.total - usage.used),
         percent=f"{usage.percent:.0f}%"
     )
